b933f0fdde6c8116a4634d677a2644f5175424a7
Paula Araujo <p.araujop13@gmail.com>
2023-12-15 10:33:36 -0300

    Conclusão desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "(11)91234-5678", modelo: "Nokia X", imei: "123456789", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "(12)98765-4321", modelo: "iPhone Y", imei: "987654321", memoria: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
